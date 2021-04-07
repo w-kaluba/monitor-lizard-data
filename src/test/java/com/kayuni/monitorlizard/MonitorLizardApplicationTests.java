abd247a1a0ddaf0fafdb4a3b0c1e77e57376e01b
Wiza Kaluba <kaluba.w@gmail.com>
2021-04-07 17:43:11 +0100

    Initialized Spring Boot application Initialized Docker configuration

---
+++ src/test/java/com/kayuni/monitorlizard/MonitorLizardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kayuni.monitorlizard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorLizardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
